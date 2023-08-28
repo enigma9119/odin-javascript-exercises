96dff3e8d65cffe6f7034dbf147b3cf71c122f75
enigma9119 <enigma9119@users.noreply.github.com>
2023-08-28 00:44:38 -0700

    Finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...elementsToRemove) {
+  // Alternate Method
+  // return arr.filter(item => !elementsToRemove.includes(item));
 
+  let result = [];
+
+  arr.forEach((item) => {
+    if (!elementsToRemove.includes(item)) {
+      result.push(item);
+    }
+  });
+
+  return result;
 };
 
 // Do not edit below this line
