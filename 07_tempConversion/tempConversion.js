6e9304c74c91ca1a49bd4c26e5cd1b347630603f
enigma9119 <enigma9119@users.noreply.github.com>
2023-08-28 19:33:22 -0700

    Finish exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,18 @@
-const convertToCelsius = function() {
+function round(value, precision = 0) {
+  let multiplier = 10 ** precision;
+  return Math.round(value * multiplier) / multiplier;
+}
+
+const convertToCelsius = function (fahrenheit) {
+  return round((fahrenheit - 32) / 1.8, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  return round(celsius * 1.8 + 32, 1);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
