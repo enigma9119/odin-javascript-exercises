df638744f7ffef976219ef2aa11b939e1b3bc91e
enigma9119 <enigma9119@users.noreply.github.com>
2023-09-01 19:15:03 -0700

    Finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  str = str.replace(/[^0-9a-z]/gi, "");
 
+  let [left, right] = [0, str.length - 1];
+  while (left < right) {
+    if (str[left].toLowerCase() !== str[right].toLowerCase()) return false;
+    left++;
+    right--;
+  }
+
+  return true;
 };
 
 // Do not edit below this line
