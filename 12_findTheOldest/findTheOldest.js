26e5609015f688fcfe3fe2dca7e4fa9cbe525b94
enigma9119 <enigma9119@users.noreply.github.com>
2023-09-01 20:00:55 -0700

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  // Time complexity = O(nlogn)
+  // const sortedPeople = people.sort((a, b) => {
+  //   a.yearOfDeath = a.yearOfDeath ?? new Date().getFullYear();
+  //   b.yearOfDeath = b.yearOfDeath ?? new Date().getFullYear();
 
+  //   const ageA = a.yearOfDeath - a.yearOfBirth;
+  //   const ageB = b.yearOfDeath - b.yearOfBirth;
+
+  //   return ageB - ageA;
+  // });
+
+  // return sortedPeople[0];
+
+  // More efficient, time complexity: O(n)
+  return people.reduce((oldest, current) => {
+    const ageCurrent = getAge(current.yearOfBirth, current.yearOfDeath);
+    const ageOldest = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+
+    if (ageCurrent > ageOldest) oldest = current;
+    return oldest;
+  });
 };
+
+function getAge(birth, death) {
+  death = death ?? new Date().getFullYear();
+  return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
