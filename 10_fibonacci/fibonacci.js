b11516b17500ee2510875003a208ba6308435aa9
enigma9119 <enigma9119@users.noreply.github.com>
2023-09-01 19:31:28 -0700

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function (nth) {
+  if (nth < 0) return "OOPS";
+  if (nth === 0) return 0;
 
+  let [prev, curr] = [1, 1];
+  for (let i = 2; i < nth; ++i) {
+    [curr, prev] = [curr + prev, curr];
+  }
+
+  return curr;
 };
 
 // Do not edit below this line
